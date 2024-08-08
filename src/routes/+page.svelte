8821768d351e270f310968bbb7358229786b9049
JoaoManoel01 <167655396+JoaoManoel01@users.noreply.github.com>
2024-08-08 19:45:47 -0300

    first-commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "../routes/home.css"
+</script>
+
+<div class="principal">
+    
+    <nav class="menu">
+        <button>Play</button>
+        <button>How to Play</button>
+        <button>About Us</button>
+    </nav>
+
+    <section class="layout">
+        <img src="/icon.png" alt="Crosswords Logo">
+    </section>
+
+</div>
+

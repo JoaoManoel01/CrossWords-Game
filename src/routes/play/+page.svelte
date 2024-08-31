36b88f961c0cd2379559b5f952b91c91d6e98f1b
Vini <cvsn@discente.ifpe.edu.br>
2024-08-30 23:01:49 -0300

    new logic to load map

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import ReturnComponent from '../../components/returnComponent/ReturnComponent.svelte';
 	import '../../styles/play.css';
+
+let mapa = [
+	[0,0,0,0,0,0,0],
+	[0,0,0,0,0,1,0],
+	[0,0,0,1,1,1,1],
+	[0,1,0,0,0,1,0],
+	[0,1,0,0,0,1,0],
+	[0,1,1,1,1,1,1],
+	[0,1,0,0,0,0,0],
+	[0,1,1,1,0,0,0]
+
+] 
 </script>
 
 <svelte:head>
@@ -26,79 +38,20 @@
 		<div class="gridpart">
 			<ReturnComponent />
 			<div class="thegrid">
+				
 				<table class="crossword">
-					<tr id="0">
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty">1</td>
-						<td class="empty"></td>
+					{#each mapa as linha}
+					<tr>
+						{#each linha as coluna}
+							{#if coluna == 0}
+							<td class="empty"></td>
+							{:else if coluna == 1}
+							<td class="filled"> <input type="text" maxlength="1" style="width: 80px; height: 80px; background-color:#025962; font-size: 52px ; text-align: center; font-family: 'IBM plex mono', monospace ; color: #ffffff;"></td>
+							{/if}
+
+						{/each}
 					</tr>
-					<tr id="0">
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-					</tr>
-					<tr id="1">
-						<td class="empty"></td>
-						<td class="empty">2</td>
-						<td class="empty">3</td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-					</tr>
-					<tr id="2">
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-					</tr>
-					<tr id="3">
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-					</tr>
-					<tr id="4">
-						<td class="empty">4</td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-					</tr>
-					<tr id="5">
-						<td class="empty"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"> </td></tr
-					>
-					<tr id="6">
-						<td class="empty">5</td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="filled"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-						<td class="empty"></td>
-					</tr>
+				{/each}
 				</table>
 			</div>
 		</div>

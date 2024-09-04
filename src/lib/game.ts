60ddb7cff91a87f877ad05c05eb683249220d83d
JoaoManoel01 <orgjm.lima@gmail.com>
2024-09-04 20:57:13 -0300

    fazendo comentarios para melhor compreensão da lógica

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,15 +1,16 @@
-class Game {
+// feito para armazenar multiplas informações dentro do jogo, mapas, respostas e os inputs.
+class Game {   			
 	mapa: number[][];
 	respostas: string[][];
 	inputs: string[][];
-
+// o constructor está sendo usado para passar os parametros e indexando os atributos da classe.
 	constructor(mapa: number[][], respostas: string[][], inputs: string[][]) {
 		this.mapa = mapa;
 		this.respostas = respostas;
 		this.inputs = inputs;
 	}
 }
-
+// a função em questão está servindo de base para construir os mapas.
 function mapas(fase: number = 1):number[][] {
 	const mapa1 = [
 		[0, 0, 0, 0, 0, 0, 0],
@@ -34,7 +35,7 @@ function mapas(fase: number = 1):number[][] {
 
 	return []
 }
-
+// a função está servindo de base para montar e armazenar as respostas.
 function respostas(fase: number = 1):string[][] {
 	const respostas1 = [
 		[' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -58,12 +59,12 @@ function respostas(fase: number = 1):string[][] {
 	}
 	return []
 }
-
+// ele está exportando os atributos do game 
 export function instanceGame(fase: number = 1) {
 	const mapa = mapas(fase);
 	const resposta = respostas(fase);
 	const inputs: string[][] = []; //variavel onde o usuário vai interajir no jogo
-
+// está montando os inputs baseado no mapa.
 	for (let i = 0; i < mapa.length; i++) {
 		inputs[i] = [];
 		for (let j = 0; j < mapa[i].length; j++) {
@@ -74,7 +75,7 @@ export function instanceGame(fase: number = 1) {
 	const game = new Game(mapa, resposta, inputs);
 	return game;
 }
-
+// função para deixar as letras Maiúsculas 
 export function formatWord(inputs: string[][]) {
 	for (let i = 0; i < inputs.length; i++) {
 		for (let j = 0; j < inputs[i].length; j++) {
@@ -84,14 +85,14 @@ export function formatWord(inputs: string[][]) {
 		}
 	}
 }
-
+// fazendo o teste das respostas do usuario e verificando se estão corretas 
 export function testWord(text: string, respostas: string) {
 	if (text === respostas) {
 		return 'green';
 	}
 	return 'red';
 }
-
+// verificando se TODAS as resposas estão corretas 
 export function wins(
 	inputs:string[][], 
 	respostas:string[][]

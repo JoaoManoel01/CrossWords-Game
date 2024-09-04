40d2b164e7663f472d23649c0469bb3cca497676
JoaoManoel01 <orgjm.lima@gmail.com>
2024-09-04 11:37:53 -0300

    logica do jogo

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,59 +1,72 @@
 <script lang="ts">
-	import ReturnComponent from '../../components/returnComponent/ReturnComponent.svelte';
-	import '../../styles/play.css';
+	import { updated } from "$app/stores";
 
-let mapa = [
-	[0,0,0,0,0,0,0],
-	[0,0,0,0,0,1,0],
-	[0,0,0,1,1,1,1],
-	[0,1,0,0,0,1,0],
-	[0,1,0,0,0,1,0],
-	[0,1,1,1,1,1,1],
-	[0,1,0,0,0,0,0],
-	[0,1,1,1,0,0,0]
+	
+	let mapa = [
+		[0,0,0,0,0,0,0],
+		[0,0,0,0,0,1,0],
+		[0,0,0,1,1,1,1],
+		[0,1,0,0,0,1,0],
+		[0,1,0,0,0,1,0],
+		[0,1,1,1,1,1,1],
+		[0,1,0,0,0,0,0],
+		[0,1,1,1,0,0,0]
+	];
 
-] 
+	const respostas = [
+		[' ',' ',' ',' ',' ',' ',' '],
+		[' ',' ',' ',' ',' ','A',' '],
+		[' ',' ',' ','M','I','L','K'],
+		[' ','L',' ',' ',' ','L',' '],
+		[' ','O',' ',' ',' ','A',' '],
+		[' ','S','P','R','I','N','G'],
+		[' ','E',' ',' ',' ',' ',' '],
+		[' ','R','E','D',' ',' ',' ']
+	];
+
+	let inputs: string[][] = []; //variavel onde o usuário vai interajir no jogo
+	for(let i = 0; i < mapa.length; i++) {
+		inputs[i] = [];
+		for(let j = 0; j < mapa[i].length; j++) {
+			inputs[i][j] = '';
+		}
+	}
+
+	function testWord(text: string, i: number, j: number ) {
+		if(text === respostas[i][j]) {
+			return 'green'
+		} 
+		return 'red';
+	}
+	$: {
+		inputs;
+		for(let i = 0; i < inputs.length; i++) {
+			for(let j = 0; j < inputs[i].length; j++){
+				if(!!inputs[i][j]){
+					inputs[i][j] = inputs[i][j].toUpperCase();
+				}
+			}
+		}
+	}
 </script>
 
-<svelte:head>
-	<title>How To Play</title>
-</svelte:head>
-
-<div class="cont">
-	<main class="howmain">
-		<div class="cluespart">
-			<div class="title1">CROSS WORDS</div>
-			<div class="cluestable">
-				<div class="clueh1"><h1>Clues Table</h1></div>
-				<ol>
-					<li>Teacher who likes J-Pop (5 ltr)</li>
-					<li>Opposite of Winner (5 ltr)</li>
-					<li>Accompanies coffee (4 ltr)</li>
-					<li>The season after winter (6 ltr)</li>
-					<li>A color of the blood (3 ltr)</li>
-				</ol>
-			</div>
-		</div>
-		<div class="line"></div>
-		<div class="gridpart">
-			<ReturnComponent />
-			<div class="thegrid">
-				
-				<table class="crossword">
-					{#each mapa as linha}
-					<tr>
-						{#each linha as coluna}
-							{#if coluna == 0}
-							<td class="empty"></td>
-							{:else if coluna == 1}
-							<td class="filled"> <input type="text" maxlength="1" style="width: 80px; height: 80px; background-color:#025962; font-size: 52px ; text-align: center; font-family: 'IBM plex mono', monospace ; color: #ffffff;"></td>
-							{/if}
-
-						{/each}
-					</tr>
-				{/each}
-				</table>
-			</div>
-		</div>
-	</main>
-</div>
+<table class="crossword">
+	{#each mapa as linha, i}
+		<tr>
+			{#each linha as coluna, j}
+				{#if coluna == 0}
+					<td class="empty"></td>
+				{:else if coluna == 1}
+					<td class="filled"> 
+						<input 
+							type="text" 
+							maxlength="1" 
+							style="width: 80px; height: 80px; background-color: {testWord(inputs[i][j], i, j)}; font-size: 52px ; text-align: center; font-family: 'IBM plex mono', monospace ; color: #ffffff;"
+							bind:value={inputs[i][j]}
+							>
+					</td>
+				{/if}
+			{/each}
+		</tr>
+	{/each}
+</table>
